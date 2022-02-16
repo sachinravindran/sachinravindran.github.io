8a8194611c2503adde3563386943bf578fe25cd1
Sachin Nair <r.sachin3@gmail.com>
2022-02-16 13:42:53 +0530

    Advanced Sevice Worker

---
+++ workbox-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	globDirectory: 'https://sachinravindran.github.io/',
+	globPatterns: [
+		'**/*.{css,js,scss,eot,svg,ttf,woff,woff2,png,jpg,html,json,md,webp}'
+	],
+	swDest: 'https://sachinravindran.github.io/sw.js',
+	ignoreURLParametersMatching: [
+		/^utm_/,
+		/^fbclid$/
+	]
+};
